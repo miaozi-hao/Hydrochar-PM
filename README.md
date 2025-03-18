7ed0f5bec201424995d7308f9a52492a8578c4e2
miaozi hao <mjuleader@gmail.com>
2025-03-18 09:35:47 +0800

    重新配置LFS并添加所有文件

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f0f6c6fc661e721fab48b25957005f27b7eb3db8fca90e123cfc814e6277eb8
+size 19
---
+++ heart_disease_predictor.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b3f87f2c4546c3fba2fc9798b02c739da1bd70909ca6f19329324b127f400b2
+size 5458
